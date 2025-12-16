a242dea01c7d86556b755d7f432d60034d06ba7c
Hasan Basri <hasannolur06@gmail.com>
2025-12-16 17:10:36 +0300

    messages table created

---
+++ database/migrations/2025_12_16_105128_create_messages.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Enums\StatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('message_id');
+            $table->text('content');
+            $table->string('phone_number', '20');
+            $table->timestamp('sent_at');
+            $table->text('error_message');
+            $table->enum('status', array_values(StatusEnum::cases()));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
